b7cc6f916b6f525158b290c20a63f3ba2169b0f8
刘寒 <763481082@qq.com>
2016-04-26 21:55:36 +0800

    tar.gz

--- uiauto/script/android_prepare.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-taskkill /F /im node.exe
-taskkill /F /im adb.exe
-adb kill-server
-ping 127.0.0.1 -n  2 >NUL
-adb start-server
-ping 127.0.0.1 -n  2 >NUL
--- uiauto/script/android_prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pkill -9 node
-pkill -9 adb
-sleep 2
-#adb kill-server
-#sleep 2
-#adb start-server
-#sleep 2
